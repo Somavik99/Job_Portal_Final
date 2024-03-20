8e4e0e7719ce8bedf158785153408cfd879df57c
mayank-shrivastva <100014914+mayank-shrivastva@users.noreply.github.com>
2024-03-20 18:50:37 +0530

    Add files via upload

---
+++ frontend/src/components/Home/PopularCategories.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  MdOutlineDesignServices,
+  MdOutlineWebhook,
+  MdAccountBalance,
+  MdOutlineAnimation,
+} from "react-icons/md";
+import { TbAppsFilled } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { GiArtificialIntelligence } from "react-icons/gi";
+import { IoGameController } from "react-icons/io5";
+
+const PopularCategories = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Graphics & Design",
+      subTitle: "305 Open Positions",
+      icon: <MdOutlineDesignServices />,
+    },
+    {
+      id: 2,
+      title: "Mobile App Development",
+      subTitle: "500 Open Positions",
+      icon: <TbAppsFilled />,
+    },
+    {
+      id: 3,
+      title: "Frontend Web Development",
+      subTitle: "200 Open Positions",
+      icon: <MdOutlineWebhook />,
+    },
+     
+  ];
+  return (
+    <div className="categories">
+      <h3>POPULAR CATEGORIES</h3>
+      <div className="banner">
+        {categories.map((element) => {
+          return (
+            <div className="card" key={element.id}>
+              <div className="icon">{element.icon}</div>
+              <div className="text">
+                <p>{element.title}</p>
+                <p>{element.subTitle}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PopularCategories;
